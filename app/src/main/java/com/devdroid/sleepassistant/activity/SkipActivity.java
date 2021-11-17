315087d9b17404ddbe21f0bf4ae56171823be2dd
gaolei <fugl@csizg.com>
2021-11-17 11:24:54 +0800

    添加最近桌面显示

---
+++ app/src/main/java/com/devdroid/sleepassistant/activity/SkipActivity.java
@@ -0,0 +1,33 @@
+package com.devdroid.sleepassistant.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.devdroid.sleepassistant.R;
+import com.devdroid.sleepassistant.base.BaseActivity;
+
+public class SkipActivity extends BaseActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_skip);
+  }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Intent intent = getIntent();
+    if(intent != null){
+      String action = intent.getStringExtra("action");
+      if("set".equals(action)) {
+        Intent inten = new Intent(this, SettingsActivity.class) ;
+        inten.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        startActivity(inten);
+      }
+    }
+  }
+
+
+
+}
