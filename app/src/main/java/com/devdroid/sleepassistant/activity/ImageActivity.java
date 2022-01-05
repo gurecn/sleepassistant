6516802d57b9583c4df4463f23b8455172724b21
gaolei <fugl@csizg.com>
2022-01-05 17:14:27 +0800

    增加微信分享

--- app/src/main/java/com/devdroid/sleepassistant/activity/ImageActivity.java
+++ app/src/main/java/com/devdroid/sleepassistant/activity/ImageActivity.java
@@ -1,26 +1,29 @@
 package com.devdroid.sleepassistant.activity;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
-import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
-
+import android.widget.LinearLayout;
 import com.devdroid.sleepassistant.R;
-
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXImageObject;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -34,20 +37,66 @@ public class ImageActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_image);
-    Intent intent = getIntent();
-    Bitmap bitmap = intent.getParcelableExtra("img");
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    if(getSupportActionBar() != null) {
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
     ImageView imageView = findViewById(R.id.iv_shici_img);
     imageView.setImageBitmap(ShiciActivity.mBitmap);
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-      save(ShiciActivity.mBitmap);
-    } else {
-      insertImage(ShiciActivity.mBitmap);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()){
+      case android.R.id.home:
+        finish();
+        break;
+      case R.id.item_feedback_send:
+        LinearLayout shareLayout = findViewById(R.id.ll_share_layout);
+        if(shareLayout.getVisibility() == View.VISIBLE){
+          shareLayout.setVisibility(View.GONE);
+        } else {
+          shareLayout.setVisibility(View.VISIBLE);
+        }
+        break;
+    }
+    return true;
+  }
+
+  @SuppressLint("NonConstantResourceId")
+  public void onClick(View view) {
+    switch (view.getId()){
+      case R.id.share_save_image:
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+          saveImage(ShiciActivity.mBitmap);
+        } else {
+          insertImage(ShiciActivity.mBitmap);
+        }
+        break;
+      case R.id.share_wechat:
+        shareWechat(SendMessageToWX.Req.WXSceneSession);
+        break;
+      case R.id.share_moment:
+        shareWechat(SendMessageToWX.Req.WXSceneTimeline);
+        break;
+      case R.id.share_dingtalk:
+        break;
+    }
+    LinearLayout shareLayout = findViewById(R.id.ll_share_layout);
+    if(shareLayout.getVisibility() == View.VISIBLE){
+      shareLayout.setVisibility(View.GONE);
     }
   }
   /**
    * 保存图片 SDK <=28
    */
-  public void save(Bitmap bitmap) {
+  public void saveImage(Bitmap bitmap) {
     String path = BASE_PATH + System.currentTimeMillis() + ".jpg";
     File directory = new File(BASE_PATH);
     if(!directory.isDirectory()){
@@ -59,7 +108,7 @@ public class ImageActivity extends AppCompatActivity {
     File file = new File(path);
     if(!file.exists()){
       try {
-        boolean createNewFile = file.createNewFile();
+        file.createNewFile();
       } catch (IOException e) {
         e.printStackTrace();
       }
@@ -95,5 +144,18 @@ public class ImageActivity extends AppCompatActivity {
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private void shareWechat(int scene) {
+    IWXAPI api = WXAPIFactory.createWXAPI(this, "wx87a479e549343d9f",false);
+    WXImageObject imgObj = new WXImageObject(ShiciActivity.mBitmap);
+    WXMediaMessage msg = new WXMediaMessage();
+    msg.mediaObject = imgObj;
+    SendMessageToWX.Req req = new SendMessageToWX.Req();
+    req.transaction = "shareimg";
+    req.message = msg;
+    req.scene = scene;
+    boolean isSend =  api.sendReq(req);
+    Log.d(TAG, "isSend:" + isSend);
   }
 }
