e66f46038ee5889f39244f656fa713aa6d64e6f4
gaolei <fugl@csizg.com>
2022-07-01 10:38:22 +0800

    修复数据泄露

--- app/src/main/java/com/devdroid/sleepassistant/service/QsServer.java
+++ app/src/main/java/com/devdroid/sleepassistant/service/QsServer.java
@@ -1,18 +1,22 @@
 package com.devdroid.sleepassistant.service;
 
 import android.content.Intent;
+import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import com.devdroid.sleepassistant.activity.MainActivity;
+import com.devdroid.sleepassistant.application.TheApplication;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class QsServer extends TileService {
   private static final String TAG = QsServer.class.getSimpleName();
   @Override
   public void onClick() {
     Log.d(TAG, "onClick");
     // 打开指定页面
-    Intent intent = new Intent(this, MainActivity.class);
+    Intent intent = new Intent(TheApplication.getAppContext(), MainActivity.class);
     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
     // 跳转指定页面，并关闭下拉菜单
     startActivityAndCollapse(intent);
